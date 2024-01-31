753a73177e0e40eaff3f66efd7c9fcb889db34cf
WrenVin <wrenvin@gmail.com>
2024-01-31 14:05:54 -0600

    Started js script

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ function displayOrders(data) {
         const card = document.createElement('div');
         card.className = 'card';
 
-        const name
+        const customer = document.createElement('h3');
+        customer.textContent = `${order.firstName}`;
+
+        card.appendChild(customer);
+        orderContainer.appendChild(card);
     })
 
 }
+
+fetch('/api/readsheet')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+      displayOrders(data);
+  })
+  .catch(error => console.error('Error:', error));
